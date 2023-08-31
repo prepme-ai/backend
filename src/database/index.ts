9c436138271937f54e27fd396b12099db8b81813
kundakcii <kundakcici@gmail.com>
2023-08-31 23:11:23 +0300

    fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_PRIVATE_KEY to FIREBASE_ADMIN_PRIVATE_KEY for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_CLIENT_EMAIL to FIREBASE_ADMIN_CLIENT_EMAIL for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_CLIENT_ID to FIREBASE_ADMIN_CLIENT_ID for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_AUTH_URI to FIREBASE_ADMIN_AUTH_URI for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_TOKEN_URI to FIREBASE_ADMIN_TOKEN_URI for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL to FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_CLIENT_X509_CERT_URL to FIREBASE_ADMIN_CLIENT_X509_CERT_URL for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_PRIVATE_KEY to FIREBASE_ADMIN_PRIVATE_KEY for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_CLIENT_EMAIL to FIREBASE_ADMIN_CLIENT_EMAIL for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_CLIENT_ID to FIREBASE_ADMIN_CLIENT_ID for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_AUTH_URI to FIREBASE_ADMIN_AUTH_URI for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_TOKEN_URI to FIREBASE_ADMIN_TOKEN_URI for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL to FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_CLIENT_X509_CERT_URL to FIREBASE_ADMIN_CLIENT_X509_CERT_URL for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_PRIVATE_KEY to FIREBASE_ADMIN_PRIVATE_KEY for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_CLIENT_EMAIL to FIREBASE_ADMIN_CLIENT_EMAIL for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_CLIENT_ID to FIREBASE_ADMIN_CLIENT_ID for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_AUTH_URI to FIREBASE_ADMIN_AUTH_URI for consistency fix(database.helpers.ts): fix typo in variable name FIRABASE_ADMIN_TOKEN_URI to FIREBASE_ADMIN_TOKEN_URI

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,20 +1,20 @@
-import mongoose from "mongoose";
-import firebaseAdmin from "firebase-admin";
-import { createFirebaseAdminConfig } from "./database.helpers";
+import mongoose from 'mongoose';
+import firebaseAdmin from 'firebase-admin';
+import { createFirebaseAdminConfig } from './database.helpers';
 
 export default async function () {
-  mongoose.connect(process.env.ATLAS_URI || "");
+  mongoose.connect(process.env.ATLAS_URI || '');
 
   const connection = mongoose.connection;
-  connection.once("open", () => console.log("Mongo is connected"));
+  connection.once('open', () => console.log('Mongo is connected'));
 
   try {
-    await firebaseAdmin.initializeApp({
+    firebaseAdmin.initializeApp({
       credential: firebaseAdmin.credential.cert(createFirebaseAdminConfig()),
     });
-    console.log("Firebase is connected");
+    console.log('Firebase is connected');
   } catch (error) {
-    console.log("Firebase not connected");
+    console.log('Firebase not connected');
     console.log(error);
   }
 }
